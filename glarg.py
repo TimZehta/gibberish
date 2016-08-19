3370e8a43128265bf2cb1f0406421edf17dd1065
Tim Zehta <tim@clockwork.net>
2016-08-19 13:38:58 -0500

    removed six dependency and updated globals to be all caps

--- glarg.py
+++ glarg.py
@@ -11,51 +11,48 @@ import math
 import random
 import string
 
-# Third-party
-from six.moves import range
-
 
 __all__ = ("entropy_per_word", "generate_word", "generate_words")
-vowel_set = set("aeiou")
-initial_consonants = list(set(string.ascii_lowercase) -
-                          # remove vowels
-                          vowel_set -
-                          # remove those easily confused with others
-                          set("cqx") |
-                          # add some crunchy clusters
-                          set("bl br ch dr fl fr gl gr kl kr pl pr sh sk sl"
-                              "sm sn sp st str sw tr".split()))
-final_consonants = list(set(string.ascii_lowercase) -
-                        # remove vowels
-                        vowel_set -
-                        # remove the confusables
-                        set("cjqsxy") |
-                        # add some crunchy clusters
-                        set("ch ct ft ld lf lk ll lm lp lt mp nd ng nk nt pt"
-                            "rm sh sk sp ss st".split()))
-vowels = list(vowel_set | set("ee oo ou ui".split()))
+VOWELS = set("aeiou")
+PRE_CONSONANTS = list(set(string.ascii_lowercase) -
+                      # remove vowels
+                      VOWELS -
+                      # remove those easily confused with others
+                      set("cqx") |
+                      # add some crunchy clusters
+                      set("bl br ch dr fl fr gl gr kl kr pl pr sh sk sl sm sn"
+                          " sp st str sw tr".split()))
+POST_CONSONANTS = list(set(string.ascii_lowercase) -
+                       # remove vowels
+                       VOWELS -
+                       # remove the confusables
+                       set("cjqsxy") |
+                       # add some crunchy clusters
+                       set("ch ct ft ld lf lk ll lm lp lt mp nd ng nk nt pt rm"
+                           " sh sk sp ss st".split()))
+FILL_VOWELS = list(VOWELS | set("ee oo ou ui".split()))
 
 
 def generate_word():
     """Returns a random consonant-vowel-consonant pseudo-word.
     """
-    return "".join(random.choice(s) for s in (initial_consonants,
-                                              vowels,
-                                              final_consonants))
+    return "".join(random.choice(s) for s in (PRE_CONSONANTS,
+                                              FILL_VOWELS,
+                                              POST_CONSONANTS))
 
 
 def generate_words(wordcount):
     """Returns a list of "wordcount" pseudo-words.
     """
-    return [generate_word() for _ in range(wordcount)]
+    return [generate_word() for _ in list(range(wordcount))]
 
 
 def entropy_per_word(wordcount, count):
     """Caclulates entropy per pseudo-word.
     """
-    entropy_initial = math.log(len(initial_consonants), 2)
-    entropy_vowels = math.log(len(vowels), 2)
-    entropy_final = math.log(len(final_consonants), 2)
+    entropy_initial = math.log(len(PRE_CONSONANTS), 2)
+    entropy_vowels = math.log(len(FILL_VOWELS), 2)
+    entropy_final = math.log(len(POST_CONSONANTS), 2)
     entropy_per_word = entropy_initial + entropy_vowels + entropy_final
     entropy_number = math.log(10, 2) * 4
     entropy_total = entropy_per_word * wordcount + entropy_number
@@ -88,7 +85,7 @@ def main():
     args = parser_setup()
     if args.verbose:
         print(entropy_per_word(args.wordcount, args.count))
-    for _ in range(0, args.count):
+    for _ in list(range(0, args.count)):
         number = "{0:04d}".format(random.randint(0, 9999))
         words = generate_words(args.wordcount)
         words.insert(1, number)
