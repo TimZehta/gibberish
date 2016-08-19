b057c5ec272b2002a32c0fc80e39afc9adfdab61
Tim Zehta <tim@clockwork.net>
2016-08-19 14:12:23 -0500

    updated setup for better python 3 compat

--- setup.py
+++ setup.py
@@ -1,29 +1,28 @@
+# vim: set fileencoding=utf-8 :
+
+"""glarg setup
+"""
+
+# Standard library
+from __future__ import absolute_import, division, print_function
 import os
-try:
-    # use setuptools if available
-    from setuptools import setup
-    kwargs = {
-        'entry_points': {'console_scripts':
-            'glarg = glarg:console_main',
-        }
-    }
-except ImportError:
-    # fall back to distutils
-    from distutils import setup
-    kwargs = {}
+
+# Third-party
+from setuptools import setup
 
 
-base_dir = os.path.dirname(os.path.abspath(__file__))
+setup_path = os.path.dirname(os.path.realpath(__file__))
+with open(os.path.join(setup_path, "README.rst"), "rb") as f:
+    long_description = f.read().decode("utf-8")
 
-setup(
-    name='Glarg',
-    description="A pseudo-word generator",
-    version='0.2',
-    author='Timid Robot Zehta',
-    author_email='tim@zehta.me',
-    url='https://github.com/TimZehta/glarg',
-    py_modules=('glarg',),
-    license='MIT License',
-    long_description=open(os.path.join(base_dir, 'README.rst')).read(),
-    **kwargs
-)
+setup(name='Glarg',
+      description="A pseudo-word generator",
+      version='0.2',
+      author='Timid Robot Zehta',
+      author_email='tim@zehta.me',
+      url='https://github.com/TimZehta/glarg',
+      py_modules=('glarg',),
+      license='MIT License',
+      long_description=long_description,
+      entry_points={'console_scripts': 'glarg = glarg:console_main'},
+      )
