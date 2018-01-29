bcc4f010dfad16c78e8cbaec6f456efee9b7fe96
Tim Zehta <tim@clockwork.net>
2018-01-28 19:51:55 -0600

    Removed 3.3 testing from Travis CI. Added classifiers to setup

--- setup.py
+++ setup.py
@@ -14,15 +14,29 @@ from setuptools import setup
 setup_path = os.path.dirname(os.path.realpath(__file__))
 with open(os.path.join(setup_path, "README.rst"), "rb") as f:
     long_description = f.read().decode("utf-8")
+classifiers = ["License :: OSI Approved :: MIT License",
+               "Natural Language :: English",
+               "Operating System :: POSIX :: Linux",
+               "Programming Language :: Python :: 2",
+               "Programming Language :: Python :: 2.7",
+               "Programming Language :: Python :: 3",
+               "Programming Language :: Python :: 3.4",
+               "Programming Language :: Python :: 3.5",
+               "Programming Language :: Python :: 3.6",
+               "Programming Language :: Python :: Implementation :: CPython"]
+]
 
-setup(name='Glarg',
-      description="A pseudo-word passphrase generator",
-      version='0.2',
-      author='Timid Robot Zehta',
+setup(author='Timid Robot Zehta',
       author_email='tim@zehta.me',
-      url='https://github.com/TimZehta/glarg',
-      py_modules=('glarg',),
+      classifiers=classifiers,
+      description="A pseudo-word passphrase generator",
+      download_url="https://github.com/TimZehta/glarg/releases",
+      entry_points={'console_scripts': 'glarg = glarg:console_main'},
+      keywords=["easy", "memorable", "passphrase", "password", "pseudo-word"]
       license='MIT License',
       long_description=long_description,
-      entry_points={'console_scripts': 'glarg = glarg:console_main'},
+      name='Glarg',
+      py_modules=('glarg',),
+      url='https://github.com/TimZehta/glarg',
+      version='0.2',
       )
